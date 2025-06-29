65112852b1aaac15f5e9f67d4dec62ecaa55b16b
jevinjojo <jevinjojo1@gmail.com>
2025-06-29 21:31:45 +0530

    pre-valid

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import tensorflow as tf 
 
-# mnist = tf.keras.datasets.mnist 
+# mnist = tf.keras.datasets.mnist
 # (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # x_train = tf.keras.utils.normalize(x_train,axis=1)
